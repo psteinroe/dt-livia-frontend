9112334668004214f997a5228333719265af6a04
DanielSzym <d.szymkowiak@gmx.net>
2018-10-18 19:00:10 +0200

    fixing the add note

--- src/components/home/NewNote.vue
+++ src/components/home/NewNote.vue
@@ -2,8 +2,8 @@
 <v-layout column>
     <div class="scroll-container">
         <v-flex xs12> 
-            <form @submit="addNote(content, title)">
-                <input v-model="title" placeholder="Title">
+            <form @submit.prevent="addNote(content, title)">
+                <input class="input-field col s12" v-model="title" placeholder="Title" required>
                 <br>
                 <textarea v-model="content" placeholder="Content"/>
                 <button type="submit">Add Note</button>
@@ -12,18 +12,7 @@
             </form>
         </v-flex>
     </div>
-    <div class="scroll-container">
-        <v-list three-line>
-            <v-list-tile
-                v-for="note in notes"
-                :key="note._id">
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{ note.title }}</v-list-tile-title>
-                         <v-list-tile-sub-title>{{ note.content }}</v-list-tile-sub-title>
-                    </v-list-tile-content>
-            </v-list-tile>
-        </v-list>
-    </div>
+    
 </v-layout>
 </template>
 
@@ -43,7 +32,7 @@ export default {
         }
     },
     firestore () {
-        console.log(firestore)
+        console.log(firestore.collection('notes'))
         return {
             notes: firestore.collection('notes').orderBy('title')
         }
@@ -53,22 +42,15 @@ export default {
             const timestamp = new Date()
             this.success = 'waiting...'
             firestore.collection('notes').add({
-                title: 'Tokyo',
-                content: 'Japan',
-                timestamp: timestamp
+                content,
+                title,
+                timestamp
             }).then(function (docRef) {
-                this.success = 'worked'
                 console.log('worked with ' + docRef.id)
             }).catch(function (error) {
                 console.error('Error writing document: ', error)
-                this.success = 'NO!'
             })
-            /* if (firestore.collection('notes').add({ content, title, timestamp })) {
-                firestore.collection('notes').add({ content, title, timestamp })
-                this.success = 'worked'
-            } else {
-                this.success = 'NO!'
-            } */
+            this.success = 'Done.'
         }
     }
 }
