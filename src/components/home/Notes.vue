cc8f5c5c6166aefe8cb348196529bcb208bfdb87
DanielSzym <d.szymkowiak@gmx.net>
2018-10-19 12:36:09 +0200

    New note button

--- src/components/home/Notes.vue
+++ src/components/home/Notes.vue
@@ -6,12 +6,12 @@
                 v-for="note in notes"
                 :key="note._id">
                     <v-list-tile-content>
-                        <v-list-tile-title>{{ note.title }}</v-list-tile-title>
-                         <v-list-tile-sub-title>{{ note.content }}</v-list-tile-sub-title>
+                        <v-list-tile-title class="title">{{ note.title }}</v-list-tile-title>
+                        <v-list-tile-sub-title class="content">{{ note.content }}</v-list-tile-sub-title>
                     </v-list-tile-content>
             </v-list-tile>
         </v-list>
-        <router-link to="/NewNote">
+        <router-link to="/NewNote" class="new-note">
             <span class="subheading font-weight-bold">New Note</span>
         </router-link>
     </div>
@@ -32,12 +32,32 @@ export default {
     },
     firestore () {
         return {
-            notes: firestore.collection('notes')
+            notes: firestore.collection('notes').orderBy('timestamp', 'desc')
         }
     }
 }
 </script>
 
 <style>
-
+.title {
+    font-size:36px;
+}
+.content {
+    font-size:14px;   
+}
+.new-note {
+    position:fixed;
+    right:3vw;
+    bottom:10vh;
+    width: 120px;
+    height: 32px;
+    
+    color: black;
+    text-decoration: none;
+    text-align: center; 
+    background-color:#FF5959;
+    color:white;
+    font-weight:bold;
+    border-radius:5px;
+}
 </style>
