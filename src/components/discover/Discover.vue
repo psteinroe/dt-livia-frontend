91e5126d50132b09a07fc07f54541162ba51caa6
ERegalado <rega_omega@hotmail.com>
2018-10-19 18:44:11 +0200

    Discover functionality implemented (No CSS)

--- src/components/discover/Discover.vue
+++ src/components/discover/Discover.vue
@@ -1,11 +1,40 @@
 <template>
 
+<v-layout column>
+	<div class="scroll-container">
+    <v-flex xs12>
+        <v-layout row wrap>			
+            <v-flex xs6 v-for="cat in cats"
+                :key="cat._id" >
+                <router-link to="/">
+					<img :src=cat.url class="img-thumbnail mx-auto d-block" style="max-width:150px;max-height:150px;" />
+                    <span class="subheading font-weight-bold">{{ cat.name }}</span>
+                </router-link>
+            </v-flex>
+        </v-layout>
+    </v-flex>
+	</div>
+</v-layout>
 </template>
 
 <script>
 
+import { firestore } from '../../services'
+
 export default {
-    name: 'Discover'
+    name: 'Discover',
+    data () {
+        return {
+            loading: false,
+            cats: [],
+            error: null
+        }
+    },
+    firestore () {
+        return {
+            cats: firestore.collection('categories')
+        }
+    }
 }
 </script>
 
